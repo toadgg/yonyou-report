0850f49a5b2c35759014924eb6c872cf2f369bdf
zhangxl <toadgg@gmail.com>
2017-04-07 14:30:48 +0800

    添加信息化专用客商统计

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,12 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $rows = DB::connection('sqlsrv')->table('TY_XXWHZJKS')
-            ->join('TY_XINXIWEIHU', 'TY_XINXIWEIHU.ty_xinxiid', '=', 'TY_XXWHZJKS.ty_xinxiid')
-            ->where('TY_XXWHZJKS.keshangname', '!=', '')
-            ->select('TY_XXWHZJKS.*', 'TY_XINXIWEIHU.name', 'TY_XINXIWEIHU.dept', 'TY_XINXIWEIHU.time', 'TY_XINXIWEIHU.nc', 'TY_XINXIWEIHU.shuoming')
-            ->get();
-        dd($rows->forPage(2, 10));
         return redirect(route('report.xyjh'));
 //        return view('home');
     }
